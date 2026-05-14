1d8b736a1cf2403a43dcef76720da52d17d829e3
finmook <apimooksarovath@gmail.com>
2026-05-14 16:14:21 +0700

    fix: backend url

--- frontend/app.py
+++ frontend/app.py
@@ -2,10 +2,11 @@ import streamlit as st
 import requests
 import pandas as pd
 import plotly.graph_objects as go
+from dotenv import load_dotenv
+import os
 
-
-API_URL = "http://127.0.0.1:8000"
-
+load_dotenv()
+API_URL = os.getenv("API_URL")
 
 st.set_page_config(
     page_title="AI Food Tracker",
